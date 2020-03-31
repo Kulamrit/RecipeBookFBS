3ccf27570cfd6e99f3c45ccc260da0324ee23feb
Kulamrit <35420334+Kulamrit@users.noreply.github.com>
2020-03-31 16:55:24 +0530

    31stmarcommit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is description','https://images.theweek.com/sites/default/files/styles/tw_image_9_4/public/istock-527028804.jpg?itok=VtQBDIcm&resize=450x200')
+    new Recipe('A Test Recipe', 'This is description','https://images.theweek.com/sites/default/files/styles/tw_image_9_4/public/istock-527028804.jpg?itok=VtQBDIcm&resize=450x200'),
+    new Recipe('A Test 2 Recipe', 'This is 2nd description','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzkltzAsC9_730ut7G4AhKSoMFoaWdyr2RTgdxULumeZ1HcnFk'),
+    new Recipe('A Test 3 Recipe', 'This is 3rd description','https://medlineplus.gov/images/HerbalMedicine.jpg')
   ];
 
   constructor() { }
